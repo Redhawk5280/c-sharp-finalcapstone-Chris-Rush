2c6630b698521c1e5de3db22c2dd86b650484b1e
Chris <macadamchris@gmail.com>
2023-12-01 12:46:16 -0500

    updated animal DAO

---
+++ dotnet/Capstone/Controllers/AnimalController.cs
@@ -0,0 +1,39 @@
+﻿using Capstone.DAO;
+using Capstone.Exceptions;
+using Capstone.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Capstone.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnimalController : ControllerBase
+    {
+        IAnimalDao animalDao;
+
+        public AnimalController(IAnimalDao animalDao)
+        {
+            this.animalDao = animalDao;
+        }
+
+        [HttpGet]
+        public ActionResult<IList<User>> GetUsers()
+        {
+            const string ErrorMessage = "There was an error";
+            ActionResult result = BadRequest(new { message = ErrorMessage });
+
+            try
+            {
+                result = Ok(animalDao.GetAnimals());
+
+            }
+            catch (DaoException)
+            {
+                result = StatusCode(500, ErrorMessage);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ dotnet/Capstone/DAO/IAnimalDao.cs
@@ -0,0 +1,12 @@
+﻿using Capstone.Models;
+using System.Collections.Generic;
+
+namespace Capstone.DAO
+{
+    public interface IAnimalDao
+    {
+        IList<Animal> GetAnimals();
+        Animal GetAnimalsById(int id);
+        Animal CreateAnimals(Animal animal);
+    }
+}
