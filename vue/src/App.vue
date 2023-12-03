cfc42960e4a6df2dba7811a068681aabad4fa746
Sho Neriki <ikirenohs@gmail.com>
2023-12-03 16:09:46 -0500

    added register and login links on App.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,7 +4,7 @@
       <div>
         <p>LOGO</p>
       </div>
-      <div>
+      <div class="nav-links">
         <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
         <router-link v-bind:to="{ name: 'volunteerForm' }">Volunteer</router-link>&nbsp;|&nbsp;
 
@@ -12,6 +12,8 @@
         <router-link v-bind:to="{name: 'register'}" >Register</router-link>&nbsp;|&nbsp;
         <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
       </div>
+
+      
     </div>
     <router-view />
   </div>
